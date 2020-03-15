7fcfd553a234832440f58bb5894c3797c0f13542
Jonas Hübotter <jonas.huebotter@gmail.com>
2020-03-15 15:59:25 +0100

    [#41] Allow use of operators as identifiers (#42)

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import path from 'path'
 export const FILE_EXTENSION = Object.freeze('.tn')
 export const TARGET_FILE_EXTENSION = Object.freeze('.js')
 
-export const KEYWORD_IDENTIFIERS = Object.freeze(['eval'])
+export const OPERATOR_REGEX = /(==|[!@$%^&*|<>~*\\\-+/.]+)=*>?/
 
 const STDLIB_PATH = path.join(__dirname, 'stdlib')
 export const DEFAULT_IMPORTS =
