46c646fca682501943563834cc8e100feb25768a
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-01-10 18:59:07 +0100

    update comment

--- src/type_inference/normalization.ts
+++ src/type_inference/normalization.ts
@@ -1,7 +1,8 @@
 import { ResolvedConstrainedType } from '../types/type_inference/constraints'
 
 /**
- * Given a type, reduce the type until it is normal.
+ * Given a type, reduce the type until it is normal (i.e. cannot be reduced
+ * further).
  */
 export const normalize = (
   type: ResolvedConstrainedType,
