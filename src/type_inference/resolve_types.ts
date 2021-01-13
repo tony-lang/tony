3129cdf564df09df39f7db3e00187c7b6857b324
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-01-13 16:00:26 +0100

    update resolveType signature

--- src/type_inference/resolve_types.ts
+++ src/type_inference/resolve_types.ts
@@ -1,12 +1,18 @@
 import { ResolvedType, Type } from '../types/type_inference/categories'
 import { TypeAssignment, TypeBinding } from '../types/analyze/bindings'
+import { ScopeWithTypes } from '../types/analyze/scopes'
+
+type State = {
+  scopes: ScopeWithTypes[]
+}
 
 /**
  * Given a stack of typed term bindings, a stack of type bindings and a type,
  * resolves that type by replacing term types and parametric types with their
  * actual type.
  */
-export const resolveType = (
+export const resolveType = <T extends State>(
+  state: T,
   typedTerms: TypeAssignment[][],
   types: TypeBinding[][],
   type: Type,
