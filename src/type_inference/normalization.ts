3028aa6e3e60f93958b0aff607c8631c6c9dd2b6
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-01-22 16:45:30 +0100

    fix eslint errors

--- src/type_inference/normalization.ts
+++ src/type_inference/normalization.ts
@@ -57,71 +57,71 @@ export const normalize = <T extends AbstractState>(
 }
 
 const handleAccessType = <T extends AbstractState>(
-  state: T,
-  type: AccessType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: AccessType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize access types yet.')
 }
 
 const handleConditionalType = <T extends AbstractState>(
-  state: T,
-  type: ConditionalType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: ConditionalType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize conditional types yet.')
 }
 
 const handleCurriedType = <T extends AbstractState>(
-  state: T,
-  type: CurriedType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: CurriedType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize curried types yet.')
 }
 
 const handleInterfaceType = <T extends AbstractState>(
-  state: T,
-  type: InterfaceType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: InterfaceType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize interface types yet.')
 }
 
 const handleIntersectionType = <T extends AbstractState>(
-  state: T,
-  type: IntersectionType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: IntersectionType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize intersection types yet.')
 }
 
 const handleObjectType = <T extends AbstractState>(
-  state: T,
-  type: ObjectType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: ObjectType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize object types yet.')
 }
 
 const handleParametricType = <T extends AbstractState>(
-  state: T,
-  type: ParametricType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: ParametricType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize parametric types yet.')
 }
 
 const handleRefinedType = <T extends AbstractState>(
-  state: T,
-  type: RefinedType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: RefinedType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize refined types yet.')
 }
 
 const handleSubtractionType = <T extends AbstractState>(
-  state: T,
-  type: SubtractionType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: SubtractionType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize subtraction types yet.')
 }
 
 const handleUnionType = <T extends AbstractState>(
-  state: T,
-  type: UnionType,
+  state: T, // eslint-disable-line @typescript-eslint/no-unused-vars
+  type: UnionType, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Answers<T, Return> => {
   throw new NotImplementedError('Cannot normalize union types yet.')
 }
